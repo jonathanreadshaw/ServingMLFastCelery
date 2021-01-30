649ec5e6e9b1ae82b4523533f867f4aa900feaa8
jonathanreadshaw <readshawjonathan>
2021-01-30 16:36:38 +0000

    Add support for remote broker and backend, removing hardcoded URIs

--- celery_task_app/worker.py
+++ celery_task_app/worker.py
@@ -1,8 +1,12 @@
+import os
 from celery import Celery
+
+BROKER_URI = os.environ['BROKER_URI']
+BACKEND_URI = os.environ['BACKEND_URI']
 
 app = Celery(
     'celery_app',
-    broker='amqp://guest:guest@localhost:5672',
-    backend='redis://localhost:6379/0',
+    broker=BROKER_URI,
+    backend=BACKEND_URI,
     include=['celery_task_app.tasks']
 )
